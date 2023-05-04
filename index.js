1831af11fe858f699ab29d8676c1dba72aa87641
Valber <valbermarcelino999@gmail.com>
2023-05-04 11:32:11 -0300

    Funcionouuuuu

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const axios = require('axios');
+const csvtojson = require('csvtojson');
 
 const app = express();
 
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR_1_lfHh365JdiBo7yjNc3x2SEbnY5iH6POOJ1MeU7FOUWaFP7g7SGrb8b7czeIcK35jjP_OqCUab8/pub?output=csv';
 
-app.get('/dados', (req, res) => {
-  axios.get(csvUrl)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      console.error(`Erro na requisição: ${error}`);
-      res.status(500).send('Erro ao buscar dados da planilha');
-    });
+app.get('/tabela', async (req, res) => {
+  try {
+    const response = await axios.get(csvUrl);
+    const data = await csvtojson().fromString(response.data);
+    res.json(data);
+  } catch (error) {
+    console.error(`Erro na requisição: ${error}`);
+    res.status(500).json({ error: 'Erro na requisição' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
