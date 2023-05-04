67bf9acb1f35193089829524f4850ab83c3d034c
Valber <valbermarcelino999@gmail.com>
2023-05-04 10:41:26 -0300

    First time that works

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const https = require('https');
-const csv = require('csv-parser');
+const request = require('request');
 
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR_1_lfHh365JdiBo7yjNc3x2SEbnY5iH6POOJ1MeU7FOUWaFP7g7SGrb8b7czeIcK35jjP_OqCUab8/pub?output=csv';
 
-https.get(csvUrl, (res) => {
-  res.pipe(csv())
-    .on('data', (row) => {
-      console.log(row);
-    })
-    .on('end', () => {
-      console.log('CSV file successfully processed');
-    });
-}).on('error', (err) => {
-  console.log('Error:', err);
+request.get(csvUrl, (error, response, body) => {
+  if (error) {
+    console.error(`Erro na requisição: ${error}`);
+    return;
+  }
+
+  console.log(body); // dados da tabela em formato CSV
 });
