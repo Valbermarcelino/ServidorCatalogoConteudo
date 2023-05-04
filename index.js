26bd57da1a7c2bdae27f990867d817d9045c77ae
Valber <valbermarcelino999@gmail.com>
2023-05-04 11:20:23 -0300

    Api functional return a cvs

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const request = require('request');
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
 
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR_1_lfHh365JdiBo7yjNc3x2SEbnY5iH6POOJ1MeU7FOUWaFP7g7SGrb8b7czeIcK35jjP_OqCUab8/pub?output=csv';
 
-request.get(csvUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Erro na requisição: ${error}`);
-    return;
-  }
+app.get('/dados', (req, res) => {
+  axios.get(csvUrl)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.error(`Erro na requisição: ${error}`);
+      res.status(500).send('Erro ao buscar dados da planilha');
+    });
+});
 
-  console.log(body); // dados da tabela em formato CSV
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
