dd17c4362e448d419690b310144bad73bbd646e8
Valber <valbermarcelino999@gmail.com>
2023-05-08 10:21:34 -0300

    Teste com fastify

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const fastify = require('fastify')();
 const axios = require('axios');
 const csvtojson = require('csvtojson');
 
-const app = express();
-
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR_1_lfHh365JdiBo7yjNc3x2SEbnY5iH6POOJ1MeU7FOUWaFP7g7SGrb8b7czeIcK35jjP_OqCUab8/pub?output=csv';
 
-app.get('/tabela', async (req, res) => {
+fastify.get('/tabela', async (request, reply) => {
   try {
     const response = await axios.get(csvUrl);
     const data = await csvtojson().fromString(response.data);
-    res.json(data);
+    reply.send(data);
   } catch (error) {
     console.error(`Erro na requisição: ${error}`);
-    res.status(500).json({ error: 'Erro na requisição' });
+    reply.status(500).send({ error: 'Erro na requisição' });
   }
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+fastify.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
