3ef34fbc3cfa25ee88c69a8fbc829abce18a0d5c
Valber <valbermarcelino999@gmail.com>
2023-06-14 08:29:40 -0300

    simple test of local request

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const fastify = require('fastify')();
+const express = require('express');
 const axios = require('axios');
 const csvtojson = require('csvtojson');
 
+const app = express();
+
 const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR_1_lfHh365JdiBo7yjNc3x2SEbnY5iH6POOJ1MeU7FOUWaFP7g7SGrb8b7czeIcK35jjP_OqCUab8/pub?output=csv';
 
-fastify.get('/tabela', async (request, reply) => {
+app.get('/tabela', async (req, res) => {
   try {
     const response = await axios.get(csvUrl);
     const data = await csvtojson().fromString(response.data);
-    reply.send(data);
+    res.json(data);
   } catch (error) {
     console.error(`Erro na requisição: ${error}`);
-    reply.status(500).send({ error: 'Erro na requisição' });
+    res.status(500).json({ error: 'Erro na requisição' });
   }
 });
 
 const PORT = process.env.PORT || 3000;
 
-fastify.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
